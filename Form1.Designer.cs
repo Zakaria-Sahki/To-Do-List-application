3175f845caeeec511d523c8240b43445df5100b0
Zakaria Sahki <zakariasahki028@gmail.com>
2025-12-09 15:02:12 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace To_Do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblToDoList = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.btnRemoveTask = new System.Windows.Forms.Button();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.tvTasks = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblToDoList
+            // 
+            this.lblToDoList.AutoSize = true;
+            this.lblToDoList.Font = new System.Drawing.Font("Lucida Calligraphy", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblToDoList.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.lblToDoList.Location = new System.Drawing.Point(560, 26);
+            this.lblToDoList.Name = "lblToDoList";
+            this.lblToDoList.Size = new System.Drawing.Size(443, 93);
+            this.lblToDoList.TabIndex = 0;
+            this.lblToDoList.Text = "To Do List";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.label1.Location = new System.Drawing.Point(1164, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Progress";
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Font = new System.Drawing.Font("Lucida Calligraphy", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercentage.ForeColor = System.Drawing.Color.NavajoWhite;
+            this.lblPercentage.Location = new System.Drawing.Point(1244, 484);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(92, 52);
+            this.lblPercentage.TabIndex = 2;
+            this.lblPercentage.Text = "0 %";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1095, 416);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(390, 51);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnAddTask.Font = new System.Drawing.Font("Lucida Calligraphy", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTask.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnAddTask.Location = new System.Drawing.Point(116, 496);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(210, 59);
+            this.btnAddTask.TabIndex = 4;
+            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.UseVisualStyleBackColor = false;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // btnRemoveTask
+            // 
+            this.btnRemoveTask.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnRemoveTask.Font = new System.Drawing.Font("Lucida Calligraphy", 14F, System.Drawing.FontStyle.Bold);
+            this.btnRemoveTask.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnRemoveTask.Location = new System.Drawing.Point(640, 752);
+            this.btnRemoveTask.Name = "btnRemoveTask";
+            this.btnRemoveTask.Size = new System.Drawing.Size(282, 59);
+            this.btnRemoveTask.TabIndex = 5;
+            this.btnRemoveTask.Text = "Remove Task";
+            this.btnRemoveTask.UseVisualStyleBackColor = false;
+            this.btnRemoveTask.Click += new System.EventHandler(this.btnRemoveTask_Click);
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(68, 430);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(307, 37);
+            this.txtTask.TabIndex = 6;
+            // 
+            // tvTasks
+            // 
+            this.tvTasks.CheckBoxes = true;
+            this.tvTasks.ContextMenuStrip = this.contextMenuStrip1;
+            this.tvTasks.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tvTasks.Location = new System.Drawing.Point(497, 146);
+            this.tvTasks.Name = "tvTasks";
+            this.tvTasks.Size = new System.Drawing.Size(547, 587);
+            this.tvTasks.TabIndex = 7;
+            this.tvTasks.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.tvTasks_AfterCheck);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmDelete});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(135, 36);
+            // 
+            // tsmDelete
+            // 
+            this.tsmDelete.Name = "tsmDelete";
+            this.tsmDelete.Size = new System.Drawing.Size(134, 32);
+            this.tsmDelete.Text = "Delete";
+            this.tsmDelete.Click += new System.EventHandler(this.tsmDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAddTask;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(1628, 944);
+            this.Controls.Add(this.tvTasks);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.btnRemoveTask);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblToDoList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 70, 25);
+            this.Text = "To Do List";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblToDoList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Button btnRemoveTask;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.TreeView tvTasks;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmDelete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+
+        int NumberOfTasks = 0;
+        int Percentage = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            tvTasks.Nodes.Add(txtTask.Text);
+            NumberOfTasks++;
+            UpdateProgress();
+            txtTask.Text = "";
+            txtTask.Focus();
+
+        }
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            
+            tvTasks.Nodes.Remove(tvTasks.SelectedNode);
+            NumberOfTasks--;
+            UpdateProgress();
+        }
+        private int CalculatePercentage() {
+
+            return 100 / NumberOfTasks;
+        }
+        private void UpdateProgress() {
+
+            Percentage = 0;
+            progressBar1.Value = 0;
+            foreach (TreeNode Node in tvTasks.Nodes) {
+
+                if (Node.Checked) { 
+
+                    Percentage += CalculatePercentage();
+                    progressBar1.Value += CalculatePercentage();
+                    progressBar1.Refresh();
+                }
+
+            }
+            lblPercentage.Text = Percentage.ToString() + " %";
+        }
+        private void UpdateProgress(bool Checked)
+        {
+            if (Checked)
+            {
+
+                Percentage += CalculatePercentage();
+                progressBar1.Value += CalculatePercentage();
+                progressBar1.Refresh();
+                IsAllChecked();
+            }
+            else {
+
+                Percentage -= CalculatePercentage();
+                progressBar1.Value -= CalculatePercentage();
+                progressBar1.Refresh();
+                IsAllNotChecked();
+            }
+
+            lblPercentage.Text = Percentage.ToString() + " %";
+        }
+        private void tvTasks_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+
+            UpdateProgress(e.Node.Checked);
+        }
+        private bool IsAllChecked() {
+
+            foreach (TreeNode Node in tvTasks.Nodes) { 
+                
+                if (!Node.Checked)
+                    return false;
+            }
+
+            Percentage = 100;
+            progressBar1.Value = 100;
+            progressBar1.Refresh();
+            return true;
+        }
+        private bool IsAllNotChecked()
+        {
+
+            foreach (TreeNode Node in tvTasks.Nodes)
+            {
+
+                if (Node.Checked)
+                    return false;
+            }
+
+            Percentage = 0;
+            progressBar1.Value = 0;
+            progressBar1.Refresh();
+            return true;
+        }
+        private void tsmDelete_Click(object sender, EventArgs e)
+        {
+            tvTasks.Nodes.Remove(tvTasks.SelectedNode);
+            NumberOfTasks--;
+            UpdateProgress();
+        }
+    }
+}
